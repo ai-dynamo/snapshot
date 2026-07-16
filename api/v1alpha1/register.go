aa824c09e09b2347c3488cfb99f665df173f7ffc
Ron Kahn <rkahn@nvidia.com>
2026-07-16 11:05:58 +0300

    fix: restore +groupName marker and sync generated deepcopy
    Signed-off-by: Ron Kahn <rkahn@nvidia.com>

--- api/v1alpha1/register.go
+++ api/v1alpha1/register.go
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// +kubebuilder:object:generate=true
+// +groupName=nvidia.com
 package v1alpha1
 
 import (
