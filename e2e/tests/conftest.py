25dfb66084d7ca97414cf3a5facdb6fbb073f454
shayasoolin <shay.asoolin@run.ai>
2026-08-04 13:19:20 +0300

    test: load e2e lifecycle fixtures

---
+++ e2e/tests/conftest.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+pytest_plugins = ("snapshot_e2e.lifecycle",)
