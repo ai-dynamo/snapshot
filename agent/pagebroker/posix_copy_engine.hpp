768067116b4d7f6b6374427fbbb91fba2f6d7b4c
Schwinn Saereesitthipitak <schwinns@nvidia.com>
2026-09-21 13:55:43 -0700

    feat(pagebroker): retain immutable sources for direct GPU restore
    Refs #238.
    
    Signed-off-by: Schwinn Saereesitthipitak <schwinns@nvidia.com>
    (cherry picked from commit d6eae2df017edd5af998af4275e1ea474e5cb43f)
    Signed-off-by: Schwinn Saereesitthipitak <schwinns@nvidia.com>

--- agent/pagebroker/posix_copy_engine.hpp
+++ agent/pagebroker/posix_copy_engine.hpp
@@ -12,6 +12,7 @@ class PosixCopyEngine final : public TransferEngine {
   TransferEngineType type() const override;
   uintmax_t RestoreSize(const StorageBackend& source) const override;
   void StageRestore(const StorageBackend& source, const Path& destination) const override;
+  FileDescriptor OpenRestoreSource(const StorageBackend& source) const override;
   void ValidateCheckpointDestination(const StorageBackend& destination) const override;
   bool CheckpointDestinationConflicts(const StorageBackend& destination) const override;
   void PublishCheckpoint(const Path& source, const StorageBackend& destination) const override;
